f7361b4e3367a39a3d3cd8060e03b84b81156647
Samy0412 <samantha.gadet@gmail.com>
2021-04-01 16:45:30 -0400

    add a accordion in favorites drawer

--- src/components/FavoritesDrawer.js
+++ src/components/FavoritesDrawer.js
@@ -13,6 +13,11 @@ import {
   Flex,
   SimpleGrid,
   Tooltip,
+  Accordion,
+  AccordionItem,
+  AccordionHeader,
+  AccordionPanel,
+  AccordionIcon,
 } from "@chakra-ui/core";
 import { FaStar } from "react-icons/fa"
 import {LaunchItem} from "./launches"
@@ -23,6 +28,8 @@ import { useDataLayerValue }  from './DataLayer'
 function FavoritesDrawer() {
 
   const [ {favorites}] = useDataLayerValue();
+
+  const favoritesCategories = Object.keys(favorites)
 
   const { isOpen, onOpen, onClose } = useDisclosure();
   const btnRef = React.useRef();
@@ -51,29 +58,43 @@ function FavoritesDrawer() {
           </Flex>
           
           <DrawerBody   marginTop="5" overflow="auto">
-            {favorites.launches?.length? (
-              <>
-          {/* for now this section has to be copied and adapted for each category of favorites that is added to the drawer */}
-           <Heading as="h3" fontSize="1.1rem" fontWeight="500" marginBottom="5">
-            Launches({favorites.launches?.length})
-           </Heading>
-           <SimpleGrid columns={1} spacing="5" >
-           {favorites.launches &&
-             favorites.launches
-             .flat()
-             .map((favorite) => (
-              <LaunchItem launch={favorite} key={favorite.flight_number} drawer/>
-            ))}
-          </SimpleGrid>
-          {/* ************ */}
-          </>
-             ):(
-            <Flex  justify="center" marginTop="50px">
-          <Heading as="h3" fontSize="1rem" fontWeight="500">
-          You have no favorites at the moment.
-          </Heading>
-          </Flex>
-          )}
+
+            <Accordion allowToggle >
+              {favoritesCategories && favoritesCategories.map((category)=> (
+                    <AccordionItem >
+                    <AccordionHeader _focus={{outline:"none"}}>
+                    <Box flex="1" textAlign="left">
+                    {category}({favorites[category]?.length})
+                    </Box>
+                    <AccordionIcon />
+                    </AccordionHeader>
+                    <AccordionPanel pb={4}>
+                      { favorites[category]?.length? (
+                        <>
+                      <SimpleGrid columns={1} spacing="5" >
+                      {/* for now this section has to be copied and adapted for each category of favorites that is added to the drawer */}
+                      {category === "launches" &&
+                      favorites[category]
+                      .flat()
+                      .map((favorite) => (
+                        <LaunchItem launch={favorite} key={favorite.flight_number} drawer/>
+                      ))}
+                       {/* ************ */}
+                      </SimpleGrid>
+                        </>
+                      ):(
+                      <Flex  justify="center" marginTop="50px">
+                      <Heading as="h3" fontSize="1rem" fontWeight="500">
+                      You have no favorites at the moment.
+                      </Heading>
+                      </Flex>
+                      )}
+                    </AccordionPanel>
+                  </AccordionItem>
+              ))}
+            
+            </Accordion>
+           
           </DrawerBody>
         </DrawerContent>
       </Drawer>
