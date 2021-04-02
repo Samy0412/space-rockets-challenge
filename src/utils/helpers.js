5c2c592facf0a6ca3237e7eb37de67e8a80983de
Samy0412 <samantha.gadet@gmail.com>
2021-04-01 22:17:52 -0400

    clean up

--- src/utils/helpers.js
+++
@@ -1,7 +0,0 @@
-export function isFavorite(launch){
-  let isFavorite = false;
-  favorites.forEach((favorite)=> {
-    if(favorite.flight_number === launch.flight_number) isFavorite = true;
-  })
-return isFavorite;
-}
